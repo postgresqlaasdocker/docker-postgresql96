d9a12af783ed18602e3368d6c5facbbd0c1320ee
Vinayak2017 <Arulvalan.Josephxavier@sap.com>
2017-11-07 08:29:02 +0000

    dev source

--- scripts/first_run.sh
+++ scripts/first_run.sh
@@ -5,16 +5,16 @@ DB=${POSTGRES_DBNAME:-}
 EXTENSIONS=${POSTGRES_EXTENSIONS:-}
 DATA_DIR=/data/postgres
 
-cd /var/lib/postgresql
+#cd /var/lib/postgresql
 # Start PostgreSQL service
-sudo -u postgres /usr/lib/postgresql/9.6/bin/postgres -D $DATA_DIR &
+sudo -i -u postgres /usr/lib/postgresql/9.6/bin/postgres -D $DATA_DIR &
 
-while ! sudo -u postgres psql -q -c "select true;"; do sleep 1; done
+while ! sudo -i -u postgres /usr/lib/postgresql/9.6/bin/psql -q -c "select true;"; do sleep 1; done
 
 # Create user
 echo "Creating user: \"$USER\"..."
-sudo -u postgres psql -q -c "DROP ROLE IF EXISTS \"$USER\";"
-sudo -u postgres psql -q <<-EOF
+sudo -i -u postgres /usr/lib/postgresql/9.6/bin/psql -q -c "DROP ROLE IF EXISTS \"$USER\";"
+sudo -i -u postgres /usr/lib/postgresql/9.6/bin/psql -q <<-EOF
     CREATE ROLE "$USER" WITH ENCRYPTED PASSWORD '$PASS';
     ALTER ROLE "$USER" WITH ENCRYPTED PASSWORD '$PASS';
     ALTER ROLE "$USER" WITH LOGIN;
@@ -25,7 +25,7 @@ EOF
 # Create dabatase
 if [ ! -z "$DB" ]; then
     echo "Creating database: \"$DB\"..."
-    sudo -u postgres psql -q <<-EOF
+    sudo -i -u postgres /usr/lib/postgresql/9.6/bin/psql -q <<-EOF
     CREATE DATABASE "$DB" WITH OWNER="$USER" ENCODING='UTF8';
     GRANT ALL ON DATABASE "$DB" TO "$USER"
 EOF
@@ -33,13 +33,13 @@ EOF
     if [[ ! -z "$EXTENSIONS" ]]; then
         for extension in $EXTENSIONS; do
             echo "Installing extension \"$extension\" for database \"$DB\"..."
-            sudo -u postgres psql -q "$DB" -c "CREATE EXTENSION \"$extension\";"
+            sudo -i -u postgres /usr/lib/postgresql/9.6/bin/psql -q "$DB" -c "CREATE EXTENSION \"$extension\";"
         done
     fi
 fi
 
 # Stop PostgreSQL service
-sudo -u postgres /usr/lib/postgresql/9.6/bin/pg_ctl stop -m fast -w -D $DATA_DIR
+sudo -i -u postgres /usr/lib/postgresql/9.6/bin/pg_ctl stop -m fast -w -D $DATA_DIR
 
 echo "========================================================================"
 echo "PostgreSQL User: \"$USER\""
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-cd /var/lib/postgresql
+#cd /var/lib/postgresql
 
 # Initialize data directory
 DATA_DIR=/data/postgres
+
+export POSTGRESQL_USER=postgres
+/usr/sbin/adduser       --system       --group       --shell /bin/bash       --disabled-password       --home /home/${POSTGRESQL_USER} ${POSTGRESQL_USER}       --gecos "Dedicated pguser user"
+touch /home/${POSTGRESQL_USER}/.bashrc
+chown ${POSTGRESQL_USER}:${POSTGRESQL_USER} /home/${POSTGRESQL_USER}/.bashrc
+
 if [ ! -f "$DATA_DIR"/postgresql.conf ]; then
     mkdir -p "$DATA_DIR"
     chown postgres:postgres "$DATA_DIR"
 
-    sudo -u postgres /usr/lib/postgresql/9.6/bin/initdb -E utf8 --locale en_US.UTF-8 -D "$DATA_DIR"
+    sudo -i -u postgres /usr/lib/postgresql/9.6/bin/initdb -E utf8 --locale en_US.UTF-8 -D "$DATA_DIR"
     sed -i -e"s/^#listen_addresses =.*$/listen_addresses = '*'/" $DATA_DIR/postgresql.conf
     echo  "shared_preload_libraries='pg_stat_statements'">> "$DATA_DIR"/postgresql.conf
     echo "host    all    all    0.0.0.0/0    md5" >> "$DATA_DIR"/pg_hba.conf
@@ -25,4 +31,4 @@ fi
 
 # Start PostgreSQL
 echo "Starting PostgreSQL..."
-exec chpst sudo -u postgres /usr/lib/postgresql/9.6/bin/postgres -D "$DATA_DIR"
+exec chpst sudo -i -u postgres /usr/lib/postgresql/9.6/bin/postgres -D "$DATA_DIR"
